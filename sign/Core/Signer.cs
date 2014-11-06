916dd719a4146d5dae5b9ca53449ff3196022858
Krzysztof Molik <kmolik@atsisa.com>
2014-11-06 14:49:50 +0100

    Update argument of attribute constructor and InternalVisibleToAttribute.

--- sign/Core/Signer.cs
+++ sign/Core/Signer.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text;
 using Mono.Cecil;
 using Sign.Core.Updater;
 
@@ -19,47 +16,47 @@ namespace Sign.Core
             {
                 new UpdateReference(),
                 new UpdateStrongNameKey(),
-                new UpdateKnowTypeAttribute(),
+                new UpdateTypeAttributeArguments(),
+                new UpdateInternalVisibleToAttribute(),
                 new UpdateBaml()
             };
         }
 
-        private static bool IsSigned(AssemblyDefinition assemblyDefinition)
+        private static bool IsSigned( AssemblyDefinition assemblyDefinition )
         {
             var name = assemblyDefinition.Name;
-            return (name.PublicKey != null && name.PublicKey.Length != 0)
-                   || (name.PublicKeyToken != null && name.PublicKeyToken.Length != 0);
+            return ( name.PublicKey != null && name.PublicKey.Length != 0 )
+                   || ( name.PublicKeyToken != null && name.PublicKeyToken.Length != 0 );
         }
 
-        public void Sign(StrongNameKeyPair snk, IAssemblyInfoProvider assemblyInfoProvider)
+        public void Sign( StrongNameKeyPair snk, IAssemblyInfoProvider assemblyInfoProvider )
         {
             var allAssemblies = assemblyInfoProvider.GetAssemblies();
-            var modified = GetNotSignedAssemblies(allAssemblies);
+            var modified = GetNotSignedAssemblies( allAssemblies );
 
-            foreach (var updater in this.updaters)
+            foreach( var updater in this.updaters )
             {
-                updater.Update(snk, modified, allAssemblies);
+                updater.Update( snk, modified, allAssemblies );
             }
 
-            WriteModifiedAssemblies(snk, modified);
+            WriteModifiedAssemblies( snk, modified );
         }
-          
 
-        private static void WriteModifiedAssemblies(StrongNameKeyPair snk, HashSet<IAssemblyInfo> modified)
+        private static void WriteModifiedAssemblies( StrongNameKeyPair snk, HashSet<IAssemblyInfo> modified )
         {
-            foreach (var assemblyInfo in modified)
+            foreach( var assemblyInfo in modified )
             {
-                assemblyInfo.Assembly.Write(assemblyInfo.FullPath, new WriterParameters {StrongNameKeyPair = snk});
+                assemblyInfo.Assembly.Write( assemblyInfo.FullPath, new WriterParameters { StrongNameKeyPair = snk } );
             }
         }
 
-        private static HashSet<IAssemblyInfo> GetNotSignedAssemblies(IEnumerable<IAssemblyInfo> allAssemblies)
+        private static HashSet<IAssemblyInfo> GetNotSignedAssemblies( IEnumerable<IAssemblyInfo> allAssemblies )
         {
             var modified = new HashSet<IAssemblyInfo>();
 
-            foreach (var assemblyInfo in allAssemblies.Where(x => !IsSigned(x.Assembly)))
+            foreach( var assemblyInfo in allAssemblies.Where( x => !IsSigned( x.Assembly ) ) )
             {
-                modified.Add(assemblyInfo);
+                modified.Add( assemblyInfo );
             }
             return modified;
         }
---
+++ sign/Core/Updater/UpdateInternalVisibleToAttribute.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Common.Logging;
+using Mono.Cecil;
+
+namespace Sign.Core.Updater
+{
+    public class UpdateInternalVisibleToAttribute : IUpdater
+    {
+        private const int ExpectedArgumentsNumber = 1;
+
+        private static readonly string AttributeFullName = typeof( InternalsVisibleToAttribute ).FullName;
+
+        private static ILog log;
+
+        private static ILog Log
+        {
+            get { return log ?? ( log = LogManager.GetCurrentClassLogger() ); }
+        }
+
+
+        public void Update(
+            StrongNameKeyPair snk,
+            HashSet<IAssemblyInfo> modified,
+            IEnumerable<IAssemblyInfo> allAssemblies )
+        {
+            foreach( var assemblyInfo in modified.ToArray() )
+            {
+                UpdateAssembly( modified, assemblyInfo );
+            }
+        }
+
+        private static void UpdateAssembly( HashSet<IAssemblyInfo> modified, IAssemblyInfo assemblyInfo )
+        {
+            foreach( var info in GetInternalVisibleToAttributes( assemblyInfo ).ToArray() )
+            {
+                if( info.ConstructorArguments.Count != ExpectedArgumentsNumber )
+                {
+                    Log.WarnFormat(
+                        "Constructor of InternalVisibleToAttribute has not expected number of arguments. Actual: {0}, expected: {1}.",
+                        info.ConstructorArguments.Count,
+                        ExpectedArgumentsNumber );
+                    continue;
+                }
+
+                var argument = GetConstructorArgument( assemblyInfo, info );
+                if( argument == null )
+                {
+                    continue;
+                }
+
+                var originalAssemblyName = ( string )argument.Value.Value;
+
+                var signedAssembly = GetMatchingSignedAssembly( modified, originalAssemblyName );
+
+                if( signedAssembly == null )
+                {
+                    Log.WarnFormat(
+                        "InternalVisibleToAttribute for assembly '{0}' will be removed.",
+                        originalAssemblyName );
+                    assemblyInfo.Assembly.CustomAttributes.Remove( info );
+                    continue;
+                }
+
+                var newArgument = new CustomAttributeArgument( argument.Value.Type, signedAssembly.Assembly.FullName );
+
+                info.ConstructorArguments.Clear();
+                info.ConstructorArguments.Add( newArgument );
+            }
+        }
+
+        private static IAssemblyInfo GetMatchingSignedAssembly(
+            IEnumerable<IAssemblyInfo> modified,
+            string originalAssemblyName )
+        {
+            var assemblyByName = modified.FirstOrDefault( s => s.Assembly.Name.Name == originalAssemblyName );
+            if( assemblyByName == null )
+            {
+                Log.WarnFormat(
+                    "Not found assembly '{0}' in signed assemblies. IternalVisibleToAttribute will be removed.",
+                    originalAssemblyName );
+
+                return null;
+            }
+
+            return assemblyByName;
+        }
+
+        private static CustomAttributeArgument? GetConstructorArgument(
+            IAssemblyInfo assemblyInfo,
+            CustomAttribute info )
+        {
+            var argumnet = info.ConstructorArguments.First();
+            if( argumnet.Type != assemblyInfo.Assembly.MainModule.TypeSystem.String )
+            {
+                Log.WarnFormat(
+                    "Argument type of constructor has invalid type> Expected string type but get '{0}.",
+                    argumnet.Type.FullName );
+
+                return null;
+            }
+            return argumnet;
+        }
+
+        private static IEnumerable<CustomAttribute> GetInternalVisibleToAttributes( IAssemblyInfo assemblyInfo )
+        {
+            var internalVisibleToAttributes = assemblyInfo.Assembly.CustomAttributes
+                    .Where( t => t.AttributeType.FullName == AttributeFullName );
+
+            return internalVisibleToAttributes;
+        }
+    }
+}
