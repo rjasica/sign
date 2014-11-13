3312be856bc7421bb99ca5f51b4490d5a639005c
Krzysztof Molik <kmolik@atsisa.com>
2014-11-13 14:17:22 +0100

    Add custom directories to search directories when try resolve assemblies

--- sign/Core/DefaultAssemblyInfoProvider.cs
+++ sign/Core/DefaultAssemblyInfoProvider.cs
@@ -16,48 +16,65 @@ namespace Sign.Core
         private readonly string[] files;
         private readonly string[] dirs;
         private readonly string pattern;
+        private readonly ReaderParameters readerParameters;
 
-        public DefaultAssemblyInfoProvider([AllowNull]string[] files, [AllowNull]string[] dirs, string pattern)
+        public DefaultAssemblyInfoProvider( [AllowNull]string[] files, [AllowNull]string[] dirs, string pattern )
         {
-            if (files == null && dirs == null)
+            if( files == null && dirs == null )
             {
-                throw new AppException("Provide directory or file name", ErrorCodes.ParserError);
+                throw new AppException( "Provide directory or file name", ErrorCodes.ParserError );
             }
 
-            this.files = files ?? new string[0];
-            this.dirs = dirs ?? new string[0];
+            this.files = files ?? new string[ 0 ];
+            this.dirs = dirs ?? new string[ 0 ];
             this.pattern = pattern;
+
+            this.readerParameters = CreateReaderParameters( this.dirs );
         }
 
         public IEnumerable<IAssemblyInfo> GetAssemblies()
         {
-            var regex = new Regex(pattern);
+            var regex = new Regex( pattern );
 
-            var paths = new List<string>(files);
+            var paths = new List<string>( files );
 
-            paths.AddRange(dirs.SelectMany(Directory.EnumerateFiles).Where(x => regex.IsMatch(x)));
+            paths.AddRange( dirs.SelectMany( Directory.EnumerateFiles ).Where( x => regex.IsMatch( x ) ) );
 
-            Log.Info("Assembly to analyze:");
-            foreach (string path in paths)
+            Log.Info( "Assembly to analyze:" );
+            foreach( string path in paths )
             {
-                Log.Info(path);
+                Log.Info( path );
             }
 
             var assemblies = new List<IAssemblyInfo>();
 
-            foreach (var path in paths)
+            foreach( var path in paths )
             {
                 try
                 {
-                    assemblies.Add(new AssemblyInfo(path,AssemblyDefinition.ReadAssembly(path)));
+                    assemblies.Add( new AssemblyInfo( path, AssemblyDefinition.ReadAssembly( path, this.readerParameters ) ) );
                 }
-                catch (Exception ex)
+                catch( Exception ex )
                 {
-                    throw new AppException(string.Format("Can not read assembly: {0}", path), ex, ErrorCodes.AssemblyReadError);
+                    throw new AppException( string.Format( "Can not read assembly: {0}", path ), ex, ErrorCodes.AssemblyReadError );
                 }
             }
 
             return assemblies;
+        }
+
+        private static ReaderParameters CreateReaderParameters( IEnumerable<string> dirs )
+        {
+            var resolver = new DefaultAssemblyResolver();
+            foreach( var dir in dirs )
+            {
+                resolver.AddSearchDirectory( dir );
+            }
+
+            return new ReaderParameters
+            {
+                AssemblyResolver = resolver,
+            };
         }
     }
 }
