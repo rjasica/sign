36f279fa7ad1f9bc7076fdf1a7354c235a117173
wpokrzepa <wpokrzepa@atsisa.com>
2014-11-25 12:36:00 +0100

    Member's attribute type argument signing.

--- tests/resources/TypeFromSignAssembly/MainProgram/Program.cs
+++ tests/resources/TypeFromSignAssembly/MainProgram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 
 namespace MainProgram
@@ -14,16 +15,33 @@ namespace MainProgram
         {
             try
             {
-                var attribute = typeof( TestClass ).GetCustomAttribute<ExampleAttribute>();
+                var embededClassType = typeof( TestClass );
+                var classAttributes = embededClassType.GetCustomAttributes<ExampleAttribute>();
 
-                var instance = Activator.CreateInstance( attribute.Type );
+                var fieldsAttributes = embededClassType.GetFields( 
+                    BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic )
+                    .SelectMany( m => m.GetCustomAttributes<ExampleAttribute>() )
+                    .Where( a => a != null );
 
-                attribute.Type.GetMethod( "Write" ).Invoke( instance, null );
+                var memberAttributes = embededClassType.GetMembers()
+                    .SelectMany( m => m.GetCustomAttributes<ExampleAttribute>() )
+                    .Where( a => a != null );
+
+                foreach( var attr in classAttributes.Concat( fieldsAttributes ).Concat( memberAttributes ) )
+                {
+                    TestAttribute( attr );
+                }
             }
             catch( Exception er )
             {
                 Console.WriteLine( "Error {0}", er );
             }
+        }
+
+        private static void TestAttribute( ExampleAttribute attribute )
+        {
+            var instance = (SignAssembly.Writer)Activator.CreateInstance( attribute.Type );
+            instance.Write();
         }
     }
 }
--- tests/resources/TypeFromSignAssembly/MainProgram/TestClass.cs
+++ tests/resources/TypeFromSignAssembly/MainProgram/TestClass.cs
@@ -1,7 +1,31 @@
-﻿namespace MainProgram
+﻿using System;
+using System.Threading;
+
+namespace MainProgram
 {
     [Example( typeof( SignAssembly.Writer ) )]
     public class TestClass
     {
+        [Example( typeof( SignAssembly.Writer ) )]
+        private int field;
+
+        [Example( typeof( SignAssembly.Writer ) )]
+        public event EventHandler Event;
+
+        [Example( typeof( SignAssembly.Writer ) )]
+        public int Property { get; set; }
+
+        [Example( typeof( SignAssembly.Writer ) )]
+        public void Method()
+        {
+            this.field++;
+            var @event = Interlocked.CompareExchange( ref this.Event, null, this.Event );
+            if( @event != null )
+            {
+                @event( this, new EventArgs() );
+            }
+
+            Console.WriteLine( this.field );
+        }
     }
 }
